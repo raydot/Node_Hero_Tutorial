60fc5fec639dd69dbbfd16319850d7c1397b34a8
Dave Kanter <moatgoat@hotmail.com>
2018-03-07 14:07:03 -0800

    More reduce examples.

--- ch01/app/calc.js
+++ ch01/app/calc.js
@@ -7,7 +7,6 @@ function sum(arr) {
 }
 
 // Examples of 'reduce' from https://medium.freecodecamp.org/reduce-f47a7da511a9
-
 // ES6
 const euroses6 = [29.76, 41.85, 46.50];
 const sumes6 = euroses6.reduce((total, amount) => total + amount)
@@ -20,5 +19,12 @@ var sumes5 = euroses5.reduce( function (total, amount) {
 });
 console.log("sumes5: " + sumes5);
 
+// Examples from https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/Reduce
+//Flatten an array of arrays
+var flattened = [[0,1],[2,3],[4,5]].reduce(
+ ( accumulator, currentValue ) => accumulator.concat(currentValue), []
+);
+
+console.log(flattened);
 
 module.exports.sum = sum
